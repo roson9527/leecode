e626946323426b8de0d70a11ecb78c52ffa3495d
罗升 <mark.luo@chainedfinance.com>
2018-10-09 15:57:01 +0800

    feat(code): Q18: 4sum

---
+++ src/python/4sum-18.py
@@ -0,0 +1,34 @@
+class Solution:
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i+1, len(nums)):
+                if j > i+1 and nums[j] == nums[j - 1]:
+                    continue
+                l = j + 1
+                r = len(nums) - 1
+                while l < r:
+                    s = nums[i] + nums[j] + nums[l] + nums[r]
+                    if s == target:
+                        res.append([nums[i], nums[j], nums[l], nums[r]])
+                        while l < r and nums[l] == nums[l + 1]:
+                            l += 1
+                        while l < r and nums[r] == nums[r - 1]:
+                            r -= 1
+                        l += 1
+                        r -= 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+
+        return res
