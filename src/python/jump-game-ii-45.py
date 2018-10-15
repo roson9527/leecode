e6cc4e385aaba3a6368ccf390f4740de11fa09ce
roson9527 <roson9527@sina.cn>
2018-10-15 10:42:14 +0800

    feat(code): Q45:jump game ii

---
+++ src/python/jump-game-ii-45.py
@@ -0,0 +1,27 @@
+class Solution:
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total = 0
+        pos = 0
+        max_pos = len(nums) - 1
+        while pos < max_pos:
+            n = nums[pos]
+            if n+pos >= max_pos:
+                pos = max_pos
+            else:
+                max_len = 0
+                max_i = 0
+                for i in range(1, n + 1):
+                    tmp = i+nums[pos+i]
+                    if max_len < tmp:
+                        max_i = i
+                        max_len = tmp
+
+                pos = pos + max_i
+
+            total += 1
+
+        return total
