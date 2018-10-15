448101633fe80e7790370e4983e0e862acd84e88
roson9527 <roson9527@sina.cn>
2018-10-15 14:14:18 +0800

    feat(code): Q47:premutations ii

--- src/python/premutations-ii-47.py
+++ src/python/premutations-ii-47.py
@@ -1,9 +1,10 @@
 class Solution:
-    def permute(self, nums):
+    def permuteUnique(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        nums.sort()
         self.ant = []
         self.perm(nums, 0, len(nums))
         return self.ant
@@ -14,6 +15,8 @@ class Solution:
         else:
             i = begin
             for n in range(begin, end):
+                if n != i and nums[n] == nums[i]:
+                    continue
                 nums[n], nums[i] = nums[i], nums[n]
                 self.perm(nums, begin+1, end)
                 nums[n], nums[i] = nums[i], nums[n]
