6215dc5c2521049f79aadcd02274546ac365fd23
roson9527 <roson9527@sina.cn>
2018-11-27 15:34:32 +0800

    feat(code): Q71: simplify path

---
+++ src/python/simplify-path-71.py
@@ -0,0 +1,26 @@
+class Solution:
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        stack = []
+        i = 0
+        res = ''
+        while i < len(path):
+            end = i+1
+            while end < len(path) and path[end] != "/":
+                end += 1
+            sub = path[i+1:end]
+            if len(sub) > 0:
+                if sub == "..":
+                    if stack != []:
+                        stack.pop()
+                elif sub != ".":
+                    stack.append(sub)
+            i = end
+        if stack == []:
+            return "/"
+        for i in stack:
+            res += "/"+i
+        return res
