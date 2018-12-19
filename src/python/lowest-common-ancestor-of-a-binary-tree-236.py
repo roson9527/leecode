da16e275f0403e8e9549900e3cefc77f8807116a
roson9527 <roson9527@sina.cn>
2018-12-19 11:11:18 +0800

    feat(code): Q236: lowesr common ancestor of a binary

---
+++ src/python/lowest-common-ancestor-of-a-binary-tree-236.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        minn = min(p.val, q.val)
+        maxn = max(p.val, q.val)
+        if root is None:
+            return root
+        if minn == root.val or root.val == maxn:
+            return root
+
+        l = self.lowestCommonAncestor(root.left, p, q)
+        r = self.lowestCommonAncestor(root.right, p, q)
+        if l and r:
+            return root
+        if l:
+            return l
+        else:
+            return r
